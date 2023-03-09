787e165143f4a2852bf8fe513375a1cf702a473c
Bianca <biancaloquete@gmail.com>
2023-03-09 10:47:11 -0300

    testa

--- Emprestimo.php
+++ Emprestimo.php
@@ -1,3 +1,10 @@
 <?php
 
+class Emprestimo extends Pessoas implements BiblioInterface {
+    public function __construct($emprestimo, $devolucao) {
+        parent::__construct($emprestimo, $devolucao);
+    }
+}
+     
+
 ?>
--- Index.php
+++ Index.php
@@ -1,3 +1,19 @@
 <?php
 
+include("BiblioInterface.php");
+include("Emprestimo.php");
+include("Livros.php");
+include("funcionarios.php");
+include("Pessoas.php");
+
+$book = new Livros(
+    "É assim que começa",
+    "Coolen Hoover"
+);
+
+$devolve = new Emprestimo(
+    "28/02/2023",
+    "14/03/2023"
+);
+
 ?>
